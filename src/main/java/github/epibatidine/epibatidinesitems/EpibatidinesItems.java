ab88c9e3b26b0983d82c3a80fd7963383d6e71cb
Epibatidine <catutorial7@gmail.com>
2024-09-01 15:32:26 +0200

    Initial commit

---
+++ src/main/java/github/epibatidine/epibatidinesitems/EpibatidinesItems.java
@@ -0,0 +1,16 @@
+package github.epibatidine.epibatidinesitems;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EpibatidinesItems implements ModInitializer {
+	public static final String MOD_ID = "epibatidinesitems";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/github/epibatidine/epibatidinesitems/EpibatidinesItemsClient.java
@@ -0,0 +1,10 @@
+package github.epibatidine.epibatidinesitems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EpibatidinesItemsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
